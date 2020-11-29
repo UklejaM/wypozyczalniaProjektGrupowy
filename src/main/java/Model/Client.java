cfd9b6bb02eba54d227d8a034e9a9e80512cc88f
DominikH13 <dominik.hubka@gmail.com>
2020-11-29 12:59:32 +0100

    z getterami seterami

--- src/main/java/Model/Client.java
+++ src/main/java/Model/Client.java
@@ -21,5 +21,75 @@ public class Client {
     private String clientMail;
     private String clientPassword;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getClientAddress() {
+        return clientAddress;
+    }
+
+    public void setClientAddress(String clientAddress) {
+        this.clientAddress = clientAddress;
+    }
+
+    public String getIdCardNumber() {
+        return IdCardNumber;
+    }
+
+    public void setIdCardNumber(String idCardNumber) {
+        IdCardNumber = idCardNumber;
+    }
+
+    public String getClientMail() {
+        return clientMail;
+    }
+
+    public void setClientMail(String clientMail) {
+        this.clientMail = clientMail;
+    }
+
+    public String getClientPassword() {
+        return clientPassword;
+    }
+
+    public void setClientPassword(String clientPassword) {
+        this.clientPassword = clientPassword;
+    }
 }
