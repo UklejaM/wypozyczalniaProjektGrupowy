23f1339ba42a20615886c47232e59944406de794
Mateusz Ukleja <ukleja.mateusz@gmail.com>
2020-12-05 10:33:40 +0100

    poprawiony Gear

--- src/main/java/Model/Gear.java
+++ src/main/java/Model/Gear.java
@@ -14,8 +14,48 @@ public class Gear {
     private Enum<GearType> type;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "RentSpot_id")
-    private RentSpot rentSpot = new RentSpot();
+    private RentSpot rentSpot;
 
+    public Gear(Integer id, Enum<GearType> type, RentSpot rentSpot) {
+        this.id = id;
+        this.type = type;
+        this.rentSpot = rentSpot;
+    }
 
+    public Gear() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Enum<GearType> getType() {
+        return type;
+    }
+
+    public void setType(Enum<GearType> type) {
+        this.type = type;
+    }
+
+    public RentSpot getRentSpot() {
+        return rentSpot;
+    }
+
+    public void setRentSpot(RentSpot rentSpot) {
+        this.rentSpot = rentSpot;
+    }
+
+    @Override
+    public String toString() {
+        return "Gear{" +
+                "id=" + id +
+                ", type=" + type +
+                ", rentSpot=" + rentSpot +
+                '}';
+    }
 
 }
